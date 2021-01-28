1f4608090944fdbbe8ad608b1eeb943155590186
Li Kangle <likangle@growingio.com>
2021-01-28 16:41:11 +0800

    feat: umd提供生成包名配置
    - 添加-n参数 指定生成包名
    - umd下若未定义name则使用文件名作为生成包名

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ program.version(version, '-v, --version', '查看工具版本号').
   helpOption('-h, --help', '查看帮助').
   option('-i, --input <input>', '输入文件', 'index.js').
   option('-o, --output <output>', '输出文件', 'dist/index.js').
-  option('-f, --format <format>', '输出的文件类型', 'umd').
+  option('-f, --format <format>', '输出的文件类型', 'iife').
+  option('-n, --name <name>', '模块名').
   option('-w, --watch', '监听文件变化').
   usage('<options>')
 
@@ -18,7 +19,8 @@ let opts = program.opts()
 const options = {
   input: opts.input || 'index.js',
   output: opts.output || 'dist/index.js',
-  format: opts.format || 'umd',
+  format: opts.format || 'iife',
+  name: opts.name,
   watch: opts.hasOwnProperty('watch')
 }
 
